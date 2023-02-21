1bf2f7f3eecb77cf2a85f9bad1cf401a80b1e668
Алена Котова <mishka061@rambler.ru>
2023-02-21 11:10:09 +0300

    add

---
+++ main.js
@@ -0,0 +1,29 @@
+let num1 = document.querySelector('.text1')
+let num2 = document.querySelector('.text2')
+let res = document.querySelector('.res')
+
+let add = document.querySelector('.addition')
+let sub = document.querySelector('.subtraction')
+let mul = document.querySelector('.multiply')
+let divis = document.querySelector('.division')
+
+add.onclick = function() {
+    let sum = +num1.value + +num2.value
+    res.innerHTML=sum
+    
+}
+sub.onclick = function() {
+    let sum = +num1.value - +num2.value
+    res.innerHTML=sum
+    
+}
+mul.onclick = function() {
+    let sum = +num1.value * +num2.value
+    res.innerHTML=sum
+    
+}
+divis.onclick = function() {
+    let sum = +num1.value - +num2.value
+    res.innerHTML=sum
+    
+}
